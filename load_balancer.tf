a4f7a0b156cd7dedb636f9a30c31c907131206b7
Daryl Weston <daryl.lee.weston@gmail.com>
2020-04-01 02:08:42 +0100

    update

--- load_balancer.tf
+++ load_balancer.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "load_balancer" {
   name               = "${var.env}-daryls-lb"
   load_balancer_type = "application"
   # security_groups    = ["${aws_security_group.elb.id}"]
-  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.private1.id]
 
   enable_deletion_protection = false
 
